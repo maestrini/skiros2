4ba7251267ed98b02adea9b06945937bc8b2eacb
bJarne <bjarne@m-tech.aau.dk>
2019-10-08 21:55:52 +0200

    Update

--- skiros2_test_lib/src/skiros2_test_lib/turtlesim_example/turtlesim_primitives.py
+++ skiros2_test_lib/src/skiros2_test_lib/turtlesim_example/turtlesim_primitives.py
@@ -18,8 +18,8 @@ import math
 class Command(SkillDescription):
     def createDescription(self):
         self.addParam("Turtle", Element("cora:Robot"), ParamTypes.Required)
-        self.addParam("Linear", float, ParamTypes.Required)
-        self.addParam("Angular", float, ParamTypes.Required)
+        self.addParam("Linear", float, ParamTypes.Required, "Linear velocity")
+        self.addParam("Angular", float, ParamTypes.Required, "Angular velocity in degrees")
 
 class command(PrimitiveBase):
     def createDescription(self):
@@ -30,10 +30,13 @@ class command(PrimitiveBase):
         msg.linear.x = linear
         msg.angular.z = math.radians(angular)
         self.pose_pub.publish(msg)
-        return msg
+
+    def onPreempt(self):
+        return self.success("Stopped")
 
     def onEnd(self):
         self._send_command(0,0)
+        return True
 
     def onStart(self):
         turtle = self.params["Turtle"].value.getProperty("tts:TurtleName").value
@@ -42,7 +45,7 @@ class command(PrimitiveBase):
 
     def execute(self):
         self._send_command(self.params["Linear"].value, self.params["Angular"].value)
-        return self.step("")
+        return self.step("Running")
 
 
 #################################################################################
@@ -53,10 +56,9 @@ class Spawn(SkillDescription):
     def createDescription(self):
         #=======Params=========
         self.addParam("Name", str, ParamTypes.Required)
-        self.addParam("X", float, ParamTypes.Required)
-        self.addParam("Y", float, ParamTypes.Required)
-        self.addParam("Rotation", float, ParamTypes.Required)
-        
+        self.addParam("X", 0.0, ParamTypes.Required)
+        self.addParam("Y", 0.0, ParamTypes.Required)
+        self.addParam("Rotation", 0.0, ParamTypes.Required)
         self.addParam("Turtle", Element("cora:Robot"), ParamTypes.Optional)
 
 class spawn(PrimitiveBase):
@@ -81,8 +83,7 @@ class spawn(PrimitiveBase):
             turtle = self._wmi.add_element(turtle)
             self.params["Turtle"].value = turtle
 
-            self.success("Spawned turtle {}".format(name))
-            
+            return self.success("Spawned turtle {}".format(name))
         return self.success("")
 
 
@@ -117,34 +118,8 @@ class monitor(PrimitiveBase):
         turtle.setData(":Position", [x, y, 0.0])
         turtle.setData(":OrientationEuler", [0.0, 0.0, theta])
         self.params["Turtle"].value = turtle
-  
+
         return self.step("")
-
-
-#################################################################################
-# Wait
-#################################################################################
-
-class Wait(SkillDescription):
-    def createDescription(self):
-        #=======Params=========
-        self.addParam("Duration", float, ParamTypes.Required)
-
-class wait(PrimitiveBase):
-    def createDescription(self):
-        self.setDescription(Wait(), self.__class__.__name__)
-
-    def onStart(self):
-        self._start = rospy.Time.now()
-        return True
-        
-    def execute(self):
-        if rospy.Time.now() < self._start + rospy.Duration(self.params["Duration"].value):
-            return self.step("")
-        return self.success("")
-
-
-
 
 
 class PoseController(SkillDescription):
@@ -158,221 +133,23 @@ class PoseController(SkillDescription):
 class pose_controller(PrimitiveBase):
     def createDescription(self):
         self.setDescription(PoseController(), self.__class__.__name__)
-        
+
     def execute(self):
         turtle = self.params["Turtle"].value
         target = self.params["Target"].value
-        
+
         turtle_pos = np.array(turtle.getData(":Position"))[:2]
         target_pos = np.array(target.getData(":Position"))[:2]
         vec = target_pos - turtle_pos
         distance = np.linalg.norm(vec)
-        
+
         turtle_rot = turtle.getData(":OrientationEuler")[2]
 
         a = vec / distance
         b = np.array([math.cos(turtle_rot), math.sin(turtle_rot)])
         angle = math.acos(a.dot(b))
-        
+
         self.params["Linear"].value = distance / 4.0
         self.params["Angular"].value = math.degrees(angle) / 2.0
-        
+
         return self.step("Vel: {} / {}".format(self.params["Linear"].value, self.params["Angular"].value))
-
-
-
-
-
-
-class Wander(SkillDescription):
-    def createDescription(self):
-        #=======Params=========
-        pass
-
-class Wander2(SkillDescription):
-    def createDescription(self):
-        #=======Params=========
-        self.addParam("Name", str, ParamTypes.Required)
-
-class TurtleFind(SkillDescription):
-    def createDescription(self):
-        #=======Params=========
-        self.addParam("Turtle", Element("sumo:Object"), ParamTypes.Optional)
-
-class TargetFollow(SkillDescription):
-    def createDescription(self):
-        #=======Params=========
-        self.addParam("Target", Element("sumo:Object"), ParamTypes.Required)
-        self.addParam("Pgain", 1, ParamTypes.Required)
-
-class TurtleSpawn(SkillDescription):
-    def createDescription(self):
-        #=======Params=========
-        self.addParam("PosX", int, ParamTypes.Required)
-        self.addParam("PosY", int, ParamTypes.Required)
-        self.addParam("Rotation", int, ParamTypes.Required)
-        self.addParam("Name", str, ParamTypes.Required)
-
-#################################################################################
-# Implementations
-#################################################################################
-
-class wander_around(PrimitiveBase):
-    """
-    """
-    counter = 0
-
-    def createDescription(self):
-        self.setDescription(Wander(), self.__class__.__name__)
-
-    def _sendCmd(self):
-        msg = Twist()
-        self.counter += 0.1
-        msg.linear.x = math.sin(self.counter)
-        self.pose_pub.publish(msg)
-        return msg
-
-    def onReset(self):
-        self.counter = 0
-
-    def onStart(self):
-        my_turtle = self.params["Robot"].value.getProperty("tts:TurtleName").value
-        self.pose_pub = rospy.Publisher(my_turtle+"/cmd_vel", Twist, queue_size=20)
-        return True
-
-    def execute(self):
-        self._sendCmd()
-        return self.step("")
-
-
-class wander_around_2(PrimitiveBase):
-    """
-    Same as wander_around except for the name of the topic that is published (it depends on the "Name" parameters and not on the "Robot" parameter)
-    """
-    counter = 0
-
-    def createDescription(self):
-        self.setDescription(Wander2(), self.__class__.__name__)
-
-    def _sendCmd(self):
-        msg = Twist()
-        self.counter += 0.1
-        msg.linear.x = math.sin(self.counter)
-        self.pose_pub.publish(msg)
-        return msg
-
-    def onReset(self):
-        self.counter = 0
-
-    def onStart(self):
-        self.pose_pub = rospy.Publisher(self.params["Name"].getValue()+"/cmd_vel", Twist, queue_size=20)
-        return True
-
-    def execute(self):
-        self._sendCmd()
-        return self.step("")
-
-
-class turtle_find(PrimitiveBase):
-    """
-    """
-    def createDescription(self):
-        self.setDescription(TurtleFind(), self.__class__.__name__)
-
-
-    def execute(self):
-        my_turtle = self.params["Robot"].value.getProperty("tts:TurtleName").value
-        other_turtle = self.params["Turtle"].value
-        #Retrieve
-        tlist = [topic.replace("/pose", "") for topic, type in rospy.get_published_topics() if type=='turtlesim/Pose' and topic.find(my_turtle)==-1]
-        if tlist:
-            other_turtle.setProperty("tts:TurtleName", tlist[0])
-            self.params["Turtle"].value = other_turtle
-            print self.params["Turtle"].value.printState(True)
-            return self.success("Detected turtle {}".format(tlist[0]))
-        #return self.step("")
-        return self.fail("", -1)
-
-class target_follow(PrimitiveBase):
-    """
-    """
-    target_pose = numpy.array([0,0,0])
-    self_pose = numpy.array([0,0,0])
-    pose_sub = None
-    pose_pub = None
-    p_gain = 0
-    counter = 0
-    go_forward = False
-
-
-    def createDescription(self):
-        self.setDescription(TargetFollow(), self.__class__.__name__)
-
-    def _otherPoseMonitor(self, msg):
-        self.target_pose = numpy.array([msg.x, msg.y, msg.theta])
-
-    def _selfPoseMonitor(self, msg):
-        self.self_pose = numpy.array([msg.x, msg.y, msg.theta])
-
-    def _sendCmd(self):
-        msg = Twist()
-        diff = numpy.array([(self.target_pose[0]-self.self_pose[0]), (self.target_pose[1]-self.self_pose[1])])
-        tan = numpy.arctan2(*diff)-numpy.pi/2
-        if tan < 0:
-            tan = (2*numpy.pi + tan)
-        norm = numpy.linalg.norm(diff)
-        theta = abs(self.self_pose[2])
-        if abs(theta-tan)>0.02 and not self.go_forward:
-            msg.angular.z = self.p_gain*(theta-tan)
-        else:
-            self.go_forward = True
-            msg.linear.x = self.p_gain*norm
-        #print "{} {}".format(tan, theta)
-        self.pose_pub.publish(msg)
-        return msg
-
-    def onReset(self):
-        self.go_forward = False
-        self.target_pose = numpy.array([0,0,0])
-        self.self_pose = numpy.array([0,0,0.05])
-
-    def onStart(self):
-        my_turtle = self.params["Robot"].value.getProperty("tts:TurtleName").value
-        other_turtle = self.params["Target"].value.getProperty("tts:TurtleName").value
-        self.opose_sub = rospy.Subscriber(other_turtle+"/pose", ts.Pose, self._otherPoseMonitor)
-        self.mpose_sub = rospy.Subscriber(my_turtle+"/pose", ts.Pose, self._selfPoseMonitor)
-        self.pose_pub = rospy.Publisher(my_turtle+"/cmd_vel", Twist, queue_size=20)
-        return True
-
-    def execute(self):
-        self.p_gain = self.params["Pgain"].getValue()
-
-        cmd = self._sendCmd()
-
-        if abs(cmd.linear.x)>0.5 or abs(cmd.angular.z)>0:
-            self.counter = 0
-            return self.step("Speed: {} {}".format(cmd.linear.x, cmd.angular.z))
-        else:
-            self.counter += 1
-            if self.counter>20:
-                return self.success("")
-        return self.step("")
-
-
-class turtle_spawn(PrimitiveBase):
-    """
-    """
-    def createDescription(self):
-        self.setDescription(TurtleSpawn(), self.__class__.__name__)
-
-    def onStart(self):
-        rospy.wait_for_service('spawn')
-        try:
-            turtle_spawner = rospy.ServiceProxy('spawn', Spawn)
-            resp = turtle_spawner(self.params["PosX"].getValue() , self.params["PosY"].getValue() , self.params["Rotation"].getValue() ,  self.params["Name"].getValue())
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
-        return True
-
-    def execute(self):
-        return self.success("turtle spawned")
--- skiros2_test_lib/src/skiros2_test_lib/turtlesim_example/turtlesim_skills.py
+++ skiros2_test_lib/src/skiros2_test_lib/turtlesim_example/turtlesim_skills.py
@@ -2,77 +2,57 @@ from skiros2_skill.core.skill import SkillDescription, SkillBase, Serial, Parall
 from skiros2_common.core.params import ParamTypes
 from skiros2_common.core.world_element import Element
 
-
-
 #################################################################################
 # Move
 #################################################################################
 
+class Wander(SkillDescription):
+    def createDescription(self):
+        self.addParam("Turtle", Element("cora:Robot"), ParamTypes.Required)
+
+class round_trip(SkillBase):
+    def createDescription(self):
+        self.setDescription(Wander(), self.__class__.__name__)
+
+    def expand(self, skill):
+        skill.setProcessor(Sequential())
+        skill(
+            self.skill("Move", "move", specify={"Linear": 2.0, "Angular": 0.0}),
+            self.skill("Move", "move", specify={"Linear": 0.0, "Angular": 90.0}),
+            self.skill("Move", "move", specify={"Linear": 2.0, "Angular": 0.0}),
+            self.skill("Move", "move", specify={"Linear": 0.0, "Angular": 90.0}),
+            self.skill("Move", "move", specify={"Linear": 2.0, "Angular": 0.0}),
+            self.skill("Move", "move", specify={"Linear": 0.0, "Angular": 90.0}),
+            self.skill("Move", "move", specify={"Linear": 2.0, "Angular": 0.0}),
+            self.skill("Move", "move", specify={"Linear": 0.0, "Angular": 90.0}),
+        )
 
 class Move(SkillDescription):
     def createDescription(self):
         self.addParam("Turtle", Element("cora:Robot"), ParamTypes.Required)
-        self.addParam("Linear", float, ParamTypes.Required)
-        self.addParam("Angular", float, ParamTypes.Required)
-        self.addParam("Duration", float, ParamTypes.Required)
+        self.addParam("Linear", 0.0, ParamTypes.Required)
+        self.addParam("Angular", 0.0, ParamTypes.Required)
+        self.addParam("Duration", 1.0, ParamTypes.Optional)
 
 class move(SkillBase):
     def createDescription(self):
         self.setDescription(Move(), self.__class__.__name__)
 
     def expand(self, skill):
-        skill.setProcessor(ParallelFs())
+        velocity = self.params["Linear"].value / self.params["Duration"].value
+        angular_velocity = self.params["Angular"].value / self.params["Duration"].value
+        skill.setProcessor(Sequential())
         skill(
-            self.skill("Command", "command"),
-            self.skill("Wait", "wait")
+            self.skill(ParallelFs())(
+                self.skill("Command", "command", specify={"Linear": velocity, "Angular": angular_velocity}),
+                self.skill("Wait", "wait", specify={"Duration": self.params["Duration"].value})
+            )
         )
-
-
-class Go(SkillDescription):
-    def createDescription(self):
-        self.addParam("Turtle", Element("cora:Robot"), ParamTypes.Required)
-        self.addParam("Duration", 1.0, ParamTypes.Required)
-        self.addParam("Distance", 1.0, ParamTypes.Required)
-
-class go(SkillBase):
-    def createDescription(self):
-        self.setDescription(Go(), self.__class__.__name__)
-
-    def expand(self, skill):
-        velocity = self.params["Distance"].value / self.params["Duration"].value
-        skill.setProcessor(ParallelFs())
-        skill(
-            self.skill("Command", "command", specify={"Linear": velocity, "Angular": 0.0}),
-            self.skill("Wait", "wait", specify={"Duration": self.params["Duration"].value})
-        )
-
-
-class Turn(SkillDescription):
-    def createDescription(self):
-        self.addParam("Turtle", Element("cora:Robot"), ParamTypes.Required)
-        self.addParam("Duration", 1.0, ParamTypes.Required)
-        self.addParam("Angle", 10.0, ParamTypes.Required)
-
-class turn(SkillBase):
-    def createDescription(self):
-        self.setDescription(Turn(), self.__class__.__name__)
-
-    def expand(self, skill):
-        velocity = self.params["Angle"].value / self.params["Duration"].value
-        skill.setProcessor(ParallelFs())
-        skill(
-            self.skill("Command", "command", specify={"Angular": velocity, "Linear": 0.0}),
-            self.skill("Wait", "wait", specify={"Duration": self.params["Duration"].value})
-        )
-
-
-
-
 
 class AttractTo(SkillDescription):
     def createDescription(self):
         self.addParam("Turtle", Element("cora:Robot"), ParamTypes.Required)
-        self.addParam("Target", Element("sumo:Object"), ParamTypes.Required)
+        self.addParam("Target", Element("cora:Robot"), ParamTypes.Required)
 
 class attract_to(SkillBase):
     def createDescription(self):
@@ -81,137 +61,22 @@ class attract_to(SkillBase):
     def expand(self, skill):
         skill.setProcessor(ParallelFs())
         skill(
-            self.skill("Monitor", "monitor", specify={"Turtle": self.params["Turtle"].value}),
-            self.skill("Monitor", "monitor", specify={"Turtle": self.params["Target"].value}),
+            self.skill("Monitor", "monitor"),
+            self.skill("Monitor", "monitor", remap={"Turtle": "Target"}),
             self.skill("PoseController", "pose_controller"),
             self.skill("Command", "command"),
             self.skill("Wait", "wait", specify={"Duration": 10000.0})
         )
 
-
-
-
-#class TurnTowards(SkillDescription):
-#    def createDescription(self):
-#        self.addParam("Name", str, ParamTypes.Required)
-#        self.addParam("Target", str, ParamTypes.Required)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################################################################################
-# Description
-#################################################################################
-
-class TurtleFindAndFollow(SkillDescription):
+class demo(SkillBase):
     def createDescription(self):
-        #=======Params=========
-        self.addParam("Turtle", Element("sumo:Object"), ParamTypes.Optional)
-
-class TurtleSpawnAndFollow(SkillDescription):
-    def createDescription(self):
-        #=======Params=========
-        self.addParam("PosX", int, ParamTypes.Required)
-        self.addParam("PosY", int, ParamTypes.Required)
-        self.addParam("Rotation", int, ParamTypes.Required)
-        self.addParam("Name", str, ParamTypes.Required)
-
-class TurtleSpawnAndWander(SkillDescription):
-    def createDescription(self):
-        #=======Params=========
-        self.addParam("PosX", int, ParamTypes.Required)
-        self.addParam("PosY", int, ParamTypes.Required)
-        self.addParam("Rotation", int, ParamTypes.Required)
-        self.addParam("Name", "turtle", ParamTypes.Required)
-
-#################################################################################
-# Implementation
-#################################################################################
-
-class patrol_and_follow(SkillBase):
-    """
-    Tree is:
-    ----->:trajectory_coordinator (|Fs|)
-    ------->:TrajectoryGenerator
-    ------->:TrajectoryConsumer
-
-    """
-    def createDescription(self):
-        self.setDescription(TurtleFindAndFollow(), self.__class__.__name__)
+        self.setDescription(AttractTo(), self.__class__.__name__)
 
     def expand(self, skill):
-        skill.setProcessor(Selector())
-        skill.addChild(self.getNode(Serial()))
-        skill.last().addChild(self.getSkill(":TurtleFind", ""))
-        skill.last().addChild(self.getSkill(":TargetFollow", ""))
-        skill.last().last().remap('Target', 'Turtle')
-        skill.addChild(self.getSkill(":Wander", ""))
-
-class stay_still_and_follow(SkillBase):
-    """
-    Tree is:
-    ----->:trajectory_coordinator (|Fs|)
-    ------->:TrajectoryGenerator
-    ------->:TrajectoryConsumer
-
-    """
-    def createDescription(self):
-        self.setDescription(TurtleFindAndFollow(), self.__class__.__name__)
-
-    def expand(self, skill):
-        skill.setProcessor(Selector())
-        skill.addChild(self.getNode(Serial()))
-        skill.last().addChild(self.getSkill(":TurtleFind", ""))
-        skill.last().addChild(self.getSkill(":TargetFollow", ""))
-        skill.last().last().remap('Target', 'Turtle')
-        skill.addChild(self.getSkill(":Wait", ""))
-
-
-class turtle_spawn_and_follow(SkillBase):
-    """
-    """
-    def createDescription(self):
-        self.setDescription(TurtleSpawnAndFollow(), self.__class__.__name__)
-
-    def expand(self, skill):
-        skill.addChild(self.getNode(Sequential()))
-        skill.last().addChild(self.getSkill(":TurtleSpawn", ""))
-        skill.last().addChild(self.getSkill(":TurtleFind", ""))
-        skill.last().addChild(self.getSkill(":TargetFollow", ""))
-        skill.last().last().remap('Target', 'Turtle')
-
-
-class turtle_spawn_and_wander(SkillBase):
-    """
-    """
-    def createDescription(self):
-        self.setDescription(TurtleSpawnAndWander(), self.__class__.__name__)
-
-    def expand(self, skill):
-        skill.addChild(self.getNode(Sequential()))
-        skill.last().addChild(self.getSkill(":TurtleSpawn", ""))
-        skill.last().addChild(self.getNode(ParallelFf()))
-        skill.last().last().addChild(self.getSkill(":Wander2", ""))
-        skill.last().last().addChild(self.getSkill(":TurtleFind", ""))
-        skill.last().last().addChild(self.getSkill(":TargetFollow", ""))
-        skill.last().last().remap('Target', 'Turtle')
+        l = "Linear{}".format(self.params["Turtle"].value.label)
+        a = "Angular{}".format(self.params["Turtle"].value.label)
+        skill.setProcessor(ParallelFf())
+        skill(
+            self.skill("Wander", "round_trip", remap={"Turtle": "Target"}),
+            self.skill("AttractTo", "attract_to", remap={"Linear": l, "Angular": a}),
+        )
