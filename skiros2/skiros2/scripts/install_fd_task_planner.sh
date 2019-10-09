2b80420f1c62eb190971a147c713b3590b353cce
bJarne <bjarne@m-tech.aau.dk>
2019-10-09 19:21:17 +0200

    Added ignore file; removed user input during planner installation

--- skiros2/skiros2/scripts/install_fd_task_planner.sh
+++ skiros2/skiros2/scripts/install_fd_task_planner.sh
@@ -5,17 +5,17 @@ tfd='tfd-src-0.4'
 echo "Installing planner..."
 
 #Navigate to install folder
-default="~/utils"
-echo "Select install folder or leave blank for default [default: $default]:"
-read folder
-if [[ $folder == "" ]]; then
-    folder=${default/"~"/$HOME}
-else
-    folder=${folder/"~"/$HOME}
-fi
+folder="./utils"
+# echo "Select install folder or leave blank for default [default: $default]:"
+# read folder
+# if [[ $folder == "" ]]; then
+#     folder=${default/"~"/$HOME}
+# else
+#     folder=${folder/"~"/$HOME}
+# fi
 mkdir -p $folder
 cd $folder
-echo $folder
+# echo $folder
 
 #Install
 wget "http://gki.informatik.uni-freiburg.de/tools/tfd/downloads/version-0.4/${tfd}.tgz"
@@ -26,10 +26,11 @@ sed -e s/"translate\/"//g -i ./downward/plan.py
 sed -e s/"preprocess\/"//g -i ./downward/plan.py
 sed -e s/"search\/"//g -i ./downward/plan.py
 ./build
-cd -
+cd - >/dev/null
 rm -r "${tfd}.tgz"
 
 #Add environment variable to bashrc
+export TFD_HOME=$(pwd)
 string="export TFD_HOME=$(pwd)"
 string2="export PATH=$""TFD_HOME/${tfd}/downward:$""TFD_HOME/${tfd}/downward/translate:$""TFD_HOME/${tfd}/downward/preprocess:$""TFD_HOME/${tfd}/downward/search:""$""PATH"
 temp=$(cat ~/.bashrc | grep "$string")
@@ -41,4 +42,4 @@ else
 	echo "No export string added to bashrc (it is already there)"
 fi
 
-source ~/.bashrc
+# source ~/.bashrc
